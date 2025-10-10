1bf737dec3ee3d4c54821b3b82099d67836c1e2e
Kevin Soenandar <kevinsoenandar@Kevins-MacBook-Air-2.local>
2025-10-10 13:06:52 +1100

    Added tests

---
+++ macros/generic_tests/unique.sql
@@ -0,0 +1,58 @@
+{% test unique(model, column_name, column_names=[], where='1=1') %}
+
+    {{ return(adapter.dispatch('test_unique', 'shared_macro')(model, column_name, column_names, where)) }}
+
+{% endtest %}
+
+{% macro default__test_unique(model, column_name, column_names=[], where='1=1') %}
+
+    {% set column_names = [column_name] if (column_names | length == 0) else column_names %}
+
+    with
+        model_to_test as (
+            select * from {{ model }}
+            where {{ where }}
+        ),
+
+        duplicate_records as (
+            {% set column_names_sql_snippet %}
+                {% for column_name in column_names %}
+                    model_to_test.{{ column_name }}
+                    {%- if not loop.last -%},{%- endif %}
+                {% endfor %}
+            {% endset %}
+
+            select
+                count(*) as record_count,
+                {{ column_names_sql_snippet }}
+            from
+                model_to_test
+            group by
+                {{ column_names_sql_snippet }}
+            having
+                count(*) > 1
+        ),
+
+        final as (
+            select
+                duplicate_records.record_count,
+                '{{ column_names | join("|") }}' as uniqueness_check,
+                model_to_test.*
+
+            from duplicate_records
+                left join model_to_test
+                    on
+                    {% for column_name in column_names %}
+                        {%- if not loop.first -%}and{%- endif %}
+                        (
+                            model_to_test.{{ column_name }} = duplicate_records.{{ column_name }}
+                                or (
+                                model_to_test.{{ column_name }} is null
+                                and duplicate_records.{{ column_name }} is null
+                            )
+                        )
+                    {% endfor %}
+        )
+
+select * from final
+{% endmacro %}
