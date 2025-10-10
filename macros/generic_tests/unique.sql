a8efb6eebf78a0d96adbccee26ca5b8a85ab6ca1
Kevin Soenandar <kevinsoenandar@Kevins-MacBook-Air-2.local>
2025-10-10 13:23:09 +1100

    updated namespace

--- macros/generic_tests/unique.sql
+++ macros/generic_tests/unique.sql
@@ -1,6 +1,6 @@
 {% test unique(model, column_name, column_names=[], where='1=1') %}
 
-    {{ return(adapter.dispatch('test_unique', 'shared_macro')(model, column_name, column_names, where)) }}
+    {{ return(adapter.dispatch('test_unique', 'dbt')(model, column_name, column_names, where)) }}
 
 {% endtest %}
 
